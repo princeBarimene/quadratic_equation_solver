ae111b8a48aff4f4386597e23c9634d47f8d9751
princeBarimene <prince.g.olubari@gmail.com>
2024-11-22 14:47:32 +0000

    added the formular header

--- main.py
+++ main.py
@@ -25,32 +25,35 @@ window.config(padx = 50, pady =25)
 
 
 # creating labels
+formular_label = Label(text=f"ax\u00B2 + bx + c", font=("Arial",30, "bold"), padx=10, pady=10)
+formular_label.grid(column = 1, row = 0, columnspan=4)
+
 a_label = Label(text="a = ")
-a_label.grid(column=0, row=0)
+a_label.grid(column=0, row=1)
 
 
 
 b_label = Label(text="b = ")
-b_label.grid(column=2, row=0)
+b_label.grid(column=2, row=1)
 
 c_label = Label(text="c = ")
-c_label.grid(column=4, row=0)
+c_label.grid(column=4, row=1)
 
 
 root_label = Label(text="roots : ")
-root_label.grid(column=0, row=2)
+root_label.grid(column=0, row=3)
 
 x1_label = Label(text=f"X\u2081 = ")
-x1_label.grid(column=1, row=2)
+x1_label.grid(column=1, row=3)
 
 x1_value_label = Label(text=0)
-x1_value_label.grid(column=2, row=2)
+x1_value_label.grid(column=2, row=3)
 
 x2_label = Label(text=f"X\u2082 = ")
-x2_label.grid(column=4, row=2)
+x2_label.grid(column=4, row=3)
 
 x2_value_label = Label(text=0)
-x2_value_label.grid(column=5, row=2)
+x2_value_label.grid(column=5, row=3)
 
 
 
@@ -58,19 +61,19 @@ x2_value_label.grid(column=5, row=2)
 #creating button
 
 calculate_button = Button(text="Calculate", command=quadratic_solver)
-calculate_button.grid(column=2,row =1, columnspan= 2)
+calculate_button.grid(column=2,row =2, columnspan= 2)
 
 
 #creating input entries
 
 a_input = Entry(width = INPUT_WIDTH)
-a_input.grid(column=1, row=0)
+a_input.grid(column=1, row=1)
 
 b_input = Entry(width = INPUT_WIDTH)
-b_input.grid(column=3, row=0)
+b_input.grid(column=3, row=1)
 
 c_input = Entry(width = INPUT_WIDTH)
-c_input.grid(column=5, row=0)
+c_input.grid(column=5, row=1)
 
 
 

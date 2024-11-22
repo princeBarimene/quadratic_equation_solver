69642f0ed12108047b3f5c8ff8c0f05ef8af9bcd
princeBarimene <prince.g.olubari@gmail.com>
2024-11-22 11:27:21 +0000

    created a function quadratic solver and was able to get the inputs submitted from a when the calculate button was clicked

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ from tkinter import *
 
 INPUT_WIDTH = 5
 
+
+def quadratic_solver():
+    a_value = float(a_input.get())
+    print(a_value)
+
+
 window = Tk()
-window.minsize(width=400, height=200)
+# window.minsize(width=400, height=200)
 window.title("Quadratic Equation Solver")
 window.config(padx = 50, pady =25)
 
@@ -24,19 +30,26 @@ c_label.grid(column=4, row=0)
 root_label = Label(text="roots : ")
 root_label.grid(column=0, row=2)
 
-x_1_label = Label(text=f"X\u2081 = ")
-x_1_label.grid(column=1, row=2)
+x1_label = Label(text=f"X\u2081 = ")
+x1_label.grid(column=1, row=2)
 
-x_2_label = Label(text=f"X\u2082 = ")
-x_2_label.grid(column=4, row=2)
+x1_value_label = Label(text=0)
+x1_value_label.grid(column=2, row=2)
+
+x2_label = Label(text=f"X\u2082 = ")
+x2_label.grid(column=4, row=2)
+
+x1_value_label = Label(text=0)
+x1_value_label.grid(column=5, row=2)
 
 
 
 
 #creating button
 
-calculate_button = Button(text="Calculate")
+calculate_button = Button(text="Calculate", command=quadratic_solver)
 calculate_button.grid(column=2,row =1, columnspan= 2)
+
 
 #creating input entries
 
